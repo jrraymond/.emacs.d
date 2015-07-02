e6d848de2cda652d66c4a51218b17dde0d56f0c7
Justin Raymond <justin.raymond.14@gmail.com>
2015-07-02 09:43:23 -0400

    some-changes

--- init.el
+++ init.el
@@ -1,3 +1,57 @@
+;; Standard libraries needed
+(require 'cl-lib)
+
+;; Packages and configs to load
+(defvar packages
+  '(haskell-mode
+    smex
+    magit)
+  "Packages whose location follows the packages/package-name/package-name.el format.")
+
+(defvar custom-load-paths
+  '("structured-haskell-mode/elisp"
+    "hindent/elisp"
+    "git-modes")
+  "Custom load paths that don't follow the normal package-name/module-name.el format.")
+
+(defvar configs
+  '("global"
+    "haskell")
+  "Configuration files that follow the config/foo.el file path format.")
+
+;; Load packages
+(cl-loop for location in custom-load-paths
+         do (add-to-list 'load-path
+                         (concat (file-name-directory (or load-file-name
+                                                          (buffer-file-name)))
+                                 "packages/"
+                                 location)))
+
+(cl-loop for name in packages
+         do (progn (unless (fboundp name)
+                     (add-to-list 'load-path
+                                  (concat (file-name-directory (or load-file-name
+                                                                   (buffer-file-name)))
+                                          "packages/"
+                                          (symbol-name name)))
+                     (require name))))
+
+(require 'shm)
+(require 'hindent)
+(require 'shm-case-split)
+(require 'shm-reformat)
+
+;; Emacs configurations
+(cl-loop for name in configs
+         do (load (concat (file-name-directory load-file-name)
+                          "config/"
+                          name ".el")))
+
+;; Mode initializations
+(smex-initialize)
+(turn-on-haskell-simple-indent)
+(load "haskell-mode-autoloads.el")
+
 ;; Setup for c0-mode
 (setq c0-root "/opt/cc0/")
 (load (concat c0-root "c0-mode/c0.el"))
@@ -16,18 +70,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-;; Haskell Mode
-(load "/usr/share/emacs23/site-lisp/haskell-mode/haskell-site-file")
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-
+;; Agda Mode
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
-
-;; Evil Mode
-(add-to-list 'load-path "~/.emacs.d/elpa/evil") ; only without ELPA/el-get
-    (require 'evil)
-        (evil-mode 1)
