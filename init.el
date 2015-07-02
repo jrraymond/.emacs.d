bc58a3d9531b65041f402d58a584defac396d6ae
Justin Raymond <justin.raymond.14@gmail.com>
2015-07-02 11:04:18 -0400

    big changes

--- init.el
+++ init.el
@@ -2,22 +2,33 @@
 (require 'cl-lib)
 
 ;; Packages and configs to load
+    ;;smex
 (defvar packages
-  '(haskell-mode
-    smex
-    magit)
+  '(evil
+    haskell-mode
+    )
   "Packages whose location follows the packages/package-name/package-name.el format.")
 
+;;"structured-haskell-mode/elisp"
+;;"hindent/elisp"
+;;"git-modes"
+;;"magit/lisp"
 (defvar custom-load-paths
-  '("structured-haskell-mode/elisp"
-    "hindent/elisp"
-    "git-modes")
+  '()
   "Custom load paths that don't follow the normal package-name/module-name.el format.")
 
 (defvar configs
-  '("global"
-    "haskell")
+  '(;;"global"
+    ;;"haskell"
+    )
   "Configuration files that follow the config/foo.el file path format.")
+
+;; zenburn themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/zenburn/")
+;; moe-theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/moe-theme/")
+(add-to-list 'load-path "~/.emacs.d/themes/moe-theme/")
+(require `moe-theme)
 
 ;; Load packages
 (cl-loop for location in custom-load-paths
@@ -36,10 +47,11 @@
                                           (symbol-name name)))
                      (require name))))
 
-(require 'shm)
-(require 'hindent)
-(require 'shm-case-split)
-(require 'shm-reformat)
+;;(require 'shm)
+;;(require 'hindent)
+;;(require 'shm-case-split)
+;;(require 'shm-reformat)
+(evil-mode 1)
 
 ;; Emacs configurations
 (cl-loop for name in configs
@@ -48,29 +60,10 @@
                           name ".el")))
 
 ;; Mode initializations
-(smex-initialize)
-(turn-on-haskell-simple-indent)
+;;(smex-initialize)
+;;(turn-on-haskell-simple-indent)
 (load "haskell-mode-autoloads.el")
 
-;; Setup for c0-mode
-(setq c0-root "/opt/cc0/")
-(load (concat c0-root "c0-mode/c0.el"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes nil)
- '(inhibit-startup-screen t)
- '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; Agda Mode
-(load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
+;;(load-file (let ((coding-system-for-read 'utf-8))
+;;                (shell-command-to-string "agda-mode locate")))
