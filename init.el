4066d0dd8a81f84272a25ce56e290aa2a8a1ef7e
Justin Raymond <justin.raymond.14@gmail.com>
2015-07-02 11:18:49 -0400

    changes

--- init.el
+++ init.el
@@ -67,3 +67,17 @@
 ;; Agda Mode
 ;;(load-file (let ((coding-system-for-read 'utf-8))
 ;;                (shell-command-to-string "agda-mode locate")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("866be962f0a48c2fe648ea23a3f3c0148e5747d05626d75b6eaa9cd55a44c592" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
